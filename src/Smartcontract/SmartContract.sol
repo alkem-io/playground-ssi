387edf60bfba27879fb419711f1c6f132bbbaae4
Roemburger <56275518+Roemburger@users.noreply.github.com>
2021-01-06 16:39:46 +0100

    Update SmartContract.sol

--- src/Smartcontract/SmartContract.sol
+++ src/Smartcontract/SmartContract.sol
@@ -1,71 +1,85 @@
 pragma solidity ^0.6.0;
 
 contract SignalAt2PartyConfirmation {
-    enum StateClient { START, PROPOSAL_SEND, PROPOSAL_AGREED, PROPOSAL_DENIED, AGREEMENT_CANCELLED, AWAITING_TRANSFER, AWAITING_DELIVERY, COMPLETE }
-    enum StateDeliverer { START, PROPOSAL_SEND, PROPOSAL_AGREED, PROPOSAL_DENIED, AWAITING_TRANSFER, AWAITING_DELIVERY, COMPLETE }
-    StateClient private currentStateClient;
-    StateDeliverer private currentStateDeliverer;
+    enum StateContract { OPEN_FOR_SOLUTIONS, SOLUTIONS_PROPOSED, PROJECT_AGREED, PROJECT_DELIVERED, WORK_CONFIRMED, TRANSFER_OF_CONTRACT, ABANDONED }
+    enum ClientAbandoned { YES }
+    enum DelivererAbandoned { YES }
+    StateContract private currentStateContract;
+    ClientAbandoned private clientAbandoned;
+    DelivererAbandoned private delivererAbandoned;
     address payable client;
     address payable deliverer;
+    address payable owner;
     string store;
     
     modifier onlyClient() {
-        require(msg.sender == client, "Only client can call this method");
+        require(msg.sender == client, "Only the client can call this method");
         _;
     }
     modifier onlyDeliverer() {
-        require(msg.sender == deliverer, "Only deliverer can call this method");
+        require(msg.sender == deliverer, "Only the deliverer can call this method");
         _;
     }
-    constructor(address payable _client, address payable _deliverer) public {
-        client = _client;
-        deliverer = _deliverer;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner of the smart contract can call this method");
+        _;
     }
     
-    function ProposalSend(string memory description) onlyClient public {
-        currentStateClient = StateClient.PROPOSAL_SEND;
+    constructor(address payable _client, address payable _deliverer, address payable _owner) public {
+        client = _client;
+        deliverer = _deliverer;
+        owner = _owner;
+    }
+    
+    function AddressesDefined() public {
+        currentStateContract = StateContract.OPEN_FOR_SOLUTIONS;
+    }
+    
+    function SolutionsProposed(string memory description) onlyClient public {
+        require(currentStateContract == StateContract.OPEN_FOR_SOLUTIONS, "Not open for solutions yet");
+        currentStateContract = StateContract.SOLUTIONS_PROPOSED;
         store = description;
     }
     
-    function DisplayProposal() onlyDeliverer public view returns (string memory){
-        require(currentStateClient == StateClient.PROPOSAL_SEND, "Client did not send a propsal yet" );
+    function ProposalDisplayed() onlyDeliverer public view returns (string memory){
+        require(currentStateContract == StateContract.SOLUTIONS_PROPOSED, "Solutions are not proposed yet");
         return store;
     }
     
-    function ProposalReply(bool agreed) public {
-        require(currentStateClient == StateClient.PROPOSAL_SEND, "Client did not send proposal yet");
+    function ProposalReplied(bool agreed) onlyDeliverer public {
+        require(currentStateContract == StateContract.SOLUTIONS_PROPOSED, "Solutions are not proposed yet");
         if (agreed) {
-            currentStateDeliverer = StateDeliverer.PROPOSAL_AGREED;
+            currentStateContract = StateContract.PROJECT_AGREED;
         }
-            else {
-                currentStateDeliverer = StateDeliverer.PROPOSAL_DENIED;
-            }
     }
     
-    function SignalAmount() onlyClient public payable {
-        require(currentStateDeliverer == StateDeliverer.PROPOSAL_AGREED, "Deliverer did not agree on proposal yet");
-        require(currentStateClient != StateClient.AGREEMENT_CANCELLED, "This smart contract is out of order");
-        currentStateClient = StateClient.AWAITING_DELIVERY;
+    function ProjectDelivered() onlyDeliverer external {
+        require(currentStateContract == StateContract.PROJECT_AGREED, "Project has not yet been agreed upon");
+        currentStateContract = StateContract.PROJECT_DELIVERED;
     }
     
-    function CancelAgreement() public {
-        require(currentStateDeliverer != StateDeliverer.COMPLETE && currentStateClient != StateClient.COMPLETE, "Cannot cancel agreement due to the completion of one parties work");
-        currentStateClient = StateClient.AGREEMENT_CANCELLED;
-        selfdestruct(client);
+    function WorkConfirmed() onlyClient external {
+        require(currentStateContract == StateContract.PROJECT_DELIVERED, "Project has not yet been delivered");
+        currentStateContract = StateContract.WORK_CONFIRMED;
     }
     
-    function WorkDelivered() onlyDeliverer external {
-        require(currentStateDeliverer == StateDeliverer.PROPOSAL_AGREED, "First accept the proposal before continuing");
-        currentStateDeliverer = StateDeliverer.COMPLETE;
-    }
-    function SatisfiedClient() onlyClient external {
-        require(currentStateDeliverer == StateDeliverer.COMPLETE, "Deliverer did not deliver their work yet");
-        currentStateClient = StateClient.COMPLETE;
-    }
-    function Transfer() payable public {
-        require(currentStateClient == StateClient.COMPLETE && currentStateDeliverer == StateDeliverer.COMPLETE,
-                "Disagreement on project completion");
+    function ContractTransferred() payable public {
+        require(currentStateContract == StateContract.WORK_CONFIRMED, "Work has not yet been confirmed");
         require(address(this).balance > 0, "There is no balance left in the smartcontract to be transfered");
         deliverer.transfer(address(this).balance);
+        currentStateContract = StateContract.TRANSFER_OF_CONTRACT;
+    }
+    
+    function ContractAbandoned() public {
+        if (msg.sender == client) {
+            clientAbandoned = ClientAbandoned.YES;
+        }
+        if (msg.sender == deliverer) {
+            delivererAbandoned = DelivererAbandoned.YES;
+        }
+        if (clientAbandoned == ClientAbandoned.YES && delivererAbandoned == DelivererAbandoned.YES) {
+            currentStateContract = StateContract.ABANDONED;
+            selfdestruct(owner);
+        }
     }
 }
