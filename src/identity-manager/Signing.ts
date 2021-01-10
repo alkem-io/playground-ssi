5ab5cabfb46ced9328e4f0040409bdb0e82cad19
Parzivalza <j.rexprive@gmail.com>
2021-01-10 16:41:36 +0100

    Signing with 'jun' method

--- src/identity-manager/Signing.ts
+++ src/identity-manager/Signing.ts
@@ -11,19 +11,17 @@ async function init() {
  
   console.log('about to create SDK instance')
   const sdk = new JolocomSDK({ storage })
- 
-  // Running sdk.init() with no arguments will:
-  // - create an identity if it doesn't exist
-  // - load the identity from storage
+  
+  const Password = readlineSync.question('Create your password:').toString();
 
-  const Bob = await sdk.createAgent('demoPassword', 'jun')
+  const Bob = await sdk.createAgent(Password, 'jun')
   console.log('Agent identity:', Bob.identityWallet.identity.did)
 
   var Input = readlineSync.question('Enter data to sign:');
   console.log(`Signing following data: ${Input}`)
   
   const data = Buffer.from(Input.toString());
-  const Signed = await Bob.identityWallet.sign(data,'demoPassword')
+  const Signed = await Bob.identityWallet.sign(data, Password)
 
   console.log('Signed input', Signed.toString())
   console.log('unsigned input', JSON.stringify(data))
